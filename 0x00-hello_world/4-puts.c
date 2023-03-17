1aec259ee98eaba459e0f13e7387480b1b69fb39
Derick Godwill <godwillderick36@gmail.com>
2023-03-17 08:58:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0.
+ */
+
+intl main(void)
+
+{
+	puts("\"Programming is like building a multiligual puzzle");
+
+	return (0);
+}
